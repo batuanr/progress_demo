cb63e65fcd737b0e6eda249c90408f5c00e9c5b5
batuanr <tuanpesi@gmail.com>
2021-12-28 10:34:24 +0700

    xong

---
+++ src/app/progress-bar/progress-bar.component.ts
@@ -0,0 +1,35 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-progress-bar',
+  templateUrl: './progress-bar.component.html',
+  styleUrls: ['./progress-bar.component.css']
+})
+export class ProgressBarComponent implements OnInit {
+  interval: number;
+  @Input() backgroundColor = '#D9D9D9';
+  @Input() progressColor = '#4CAF50';
+  @Input() progress = 0;
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  loading() {
+    this.interval = setInterval(() => {
+      this.progress = this.progress + 5;
+      if (this.progress == 100) {
+        clearInterval(this.interval);
+      }
+    }, 1000);
+  }
+
+  restart(){
+    this.progress = 0;
+  }
+   stop(){
+    clearInterval(this.interval)
+   }
+
+}
